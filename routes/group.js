d15e12effdacd1268ec5fda7bc590fe0b0712e68
indrajits.int <indrajits.int@media.net>
2021-04-17 21:17:00 +0530

    groups

--- routes/group.js
+++ routes/group.js
@@ -17,13 +17,13 @@ router.route('/createGroup').post((req, res) => {
   });
 });
 router.route('/listGroup').get(async (req, res) => {
-  let result = await new Promise((resolve, reject) => {
+  const userId = req.query.userId;
+  let resu = await new Promise((resolve, reject) => {
     const sqlQuery = `SELECT groupTable.Id, groupTable.groupName 
                     FROM groupTable
                     JOIN userAccess
                     ON groupTable.id = userAccess.groupId
-                    WHERE userAccess.userId = ${req.params.userId} OR groupTable.createdByUser = ${req.params.userId}`;
-
+                    WHERE userAccess.userId = '${userId}' OR groupTable.createdByUser = '${userId}'`;
     sql.query(sqlQuery, req.body, function (err, result) {
       if (err) {
         res.status(400).send({error: err.sqlMessage});
@@ -31,23 +31,8 @@ router.route('/listGroup').get(async (req, res) => {
       }
       resolve(result);
     });
-
-    res.status(200).send({data: result});
   });
-
-  sql.query(sqlQuery, req.body, function (err, result) {
-    if (err) {
-      res.status(400).send({error: err.sqlMessage});
-      return;
-    }
-    console.log(
-      'Number of records inserted: ' +
-        result.affectedRows +
-        ' ' +
-        result.insertId,
-    );
-    res.sendStatus(201);
-  });
+  res.status(200).send({data: resu});
 });
 router.route('/signup').post((req, res) => {
   console.log(req.body);
@@ -103,46 +88,6 @@ router.route('/login').post((req, res) => {
       // res.cookie("token", token, { httpOnly: false }).sendStatus(200);
       res.status(200).json(payload);
     });
-  });
-});
-
-router.route('/getAllOrgs/:userId').get(withAuth, (req, res) => {
-  const userId = req.params.userId;
-  var sqlQuery = '';
-  console.log(res.userType);
-  switch (res.userType) {
-    case 0:
-      sqlQuery = `SELECT *
-                  FROM organization 
-                  WHERE organization.createdByUser=?`;
-      break;
-    case 1:
-      sqlQuery = `SELECT *
-                  FROM organization 
-                  INNER JOIN 
-                  teacherAccess ON organization.id = teacherAccess.organizationId
-                  WHERE teacherAccess.teacherEmail=?`;
-      break;
-    case 2:
-      sqlQuery = `SELECT *
-                  FROM organization 
-                  INNER JOIN 
-                  (SELECT classroom.organizationId FROM
-                  classroom JOIN studentAccess 
-                  ON classroom.id = studentAccess.classroomId
-                  WHERE studentAccess.studentEmail=?) classroomStudent
-                  ON organization.id = classroomStudent.organizationId`;
-      break;
-  }
-
-  console.log(sqlQuery);
-  sql.query(sqlQuery, userId, function (err, result) {
-    if (err) {
-      console.log(err);
-      res.sendStatus(400);
-      return;
-    }
-    res.status(200).json({data: result});
   });
 });
 
