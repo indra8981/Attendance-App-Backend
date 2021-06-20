ce61ad00fa62a703dc2178ebf54e7d7af6d66010
aman-dunzo <amansingh@dunzo.in>
2021-06-20 21:32:02 +0530

    fix backend for attendance logging

---
+++ utils/common_utils.js
@@ -0,0 +1,20 @@
+const constants = require('../service/constants.js');
+
+function distance(lat1, lon1, lat2, lon2) {
+  lon1 = (lon1 * Math.PI) / 180;
+  lon2 = (lon2 * Math.PI) / 180;
+  lat1 = (lat1 * Math.PI) / 180;
+  lat2 = (lat2 * Math.PI) / 180;
+
+  let dlon = lon2 - lon1;
+  let dlat = lat2 - lat1;
+  let a =
+    Math.pow(Math.sin(dlat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+  let c = 2 * Math.asin(Math.sqrt(a));
+  let r = constants.EARTH_RADIUS_IN_METRES;
+  return c * r;
+}
+
+module.exports = {distance};
